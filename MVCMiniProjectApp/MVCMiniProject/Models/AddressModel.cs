747d6614c08d1c277f7fcf6f4adea8128e317bc7
PaulRevillIgnite <paul.revill@ignite.systems>
2025-07-10 11:40:13 +0100

    MVC Mini Project

---
+++ MVCMiniProjectApp/MVCMiniProject/Models/AddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMiniProject.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        [Display(Name = "House Number")]
+        public string HouseNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Post Code")]
+        [StringLength(8, MinimumLength = 5)]
+        public string Postcode { get; set; }
+    }
+}
