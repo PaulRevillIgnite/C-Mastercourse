c07984dd69a50c48f12d8a5623718d5131c82b26
PaulRevillIgnite <paul.revill@ignite.systems>
2025-10-08 10:53:27 +0100

    Linq Lambda homework

---
+++ Linq&LambdaHomeworkSolution/Linq&LambdaHomework/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_LambdaHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataQueries();
+
+            Console.WriteLine("\nDone Processing");
+            Console.ReadLine();
+        }
+
+        public static void DataQueries()
+        {
+            var people = SampleData.GetContactData();
+
+            var results = people.Where(p => p.LastName == "Revill");
+            Console.WriteLine("People with a lastname of Revill:");
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} {item.Age}");
+            }
+
+            var results2 = people.Where(p => p.Age > 20);
+            Console.WriteLine("\nPeople over 20 years old:");
+            foreach (var item in results2)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} {item.Age}");
+            }
+
+            var results3 = (from p in people
+                            where p.FirstName == "Coral"
+                            select p);
+            Console.WriteLine("\nPeople with a firstname of Coral:");
+            foreach (var item in results3)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} {item.Age}");
+            }
+        }
+    }
+}
---
+++ Linq&LambdaHomeworkSolution/Linq&LambdaHomework/Sampledata.cs
@@ -0,0 +1,25 @@
+﻿using Linq_LambdaHomework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_LambdaHomework
+{
+    public static class SampleData
+    {
+        public static List<PersonModel> GetContactData()
+        {
+            List<PersonModel> output = new List<PersonModel>
+            {
+                new PersonModel { Id = 1, FirstName = "Paul", LastName = "Revill", Age = 33 },
+                new PersonModel { Id = 2, FirstName = "Coral", LastName = "Murphy", Age = 40 },
+                new PersonModel { Id = 3, FirstName = "Joe", LastName = "Burns", Age = 66 },
+                new PersonModel { Id = 4, FirstName = "Michael", LastName = "Dean", Age = 9 }
+            };
+
+            return output;
+        }
+    }
+}
