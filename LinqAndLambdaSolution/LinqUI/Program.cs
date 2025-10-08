86b3f139dbb4a52a9f5bc5e978c3fac8922cdc8b
PaulRevillIgnite <paul.revill@ignite.systems>
2025-10-08 10:23:39 +0100

    Linq and Lambda

---
+++ LinqAndLambdaSolution/LinqUI/Program.cs
@@ -0,0 +1,118 @@
+﻿using LinqUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LambdaTests();
+            LinqTests();
+
+            Console.WriteLine("\nDone Processing");
+            Console.ReadLine();
+        }
+
+        private static void LinqTests()
+        {
+            var contacts = SampleData.GetContactData();
+            var addresses = SampleData.GetAddressData();
+
+            //var results = (from c in contacts
+            //               where c.Address.Count > 1
+            //               select c);
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+            //}
+
+            //var results = (from c in contacts
+            //               join a in addresses on c.Id equals a.ContactId
+            //               select new {c.FirstName, c.LastName, a.City, a.State});
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName} from {item.City}, {item.State}");
+            //}
+
+            //var results = (from c in contacts
+            //               select new { c.FirstName, c.LastName, Addresses = addresses.Where(a => a.ContactId == c.Id) });
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Addresses.Count()}");
+            //}
+
+            var results = (from c in contacts
+                           select new { c.FirstName, c.LastName, Addresses = addresses.Where(a => c.Address.Contains(a.Id)) });
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} - {item.Addresses.Count()}");
+            }
+        }
+
+        private static void LambdaTests()
+        {
+            var data = SampleData.GetContactData();
+
+            //var results = data.Where(c => c.Address.Count > 1);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+            //}
+
+            //var results = data.Select(x => x.FirstName);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var results = data.Take(2);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+            //}
+
+            //var results = data.Skip(2).Take(2);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+            //}
+
+            //var results = data.OrderBy(x => x.LastName);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+            //}
+
+            //var results = data.OrderByDescending(x => x.LastName);
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+            //}
+        }
+
+        /*This line:
+            var results = data.Where(x => x.Address.Count > 1);
+
+        Is kinda the same as this method:
+            private static bool RunMe(ContactModel x)
+            {
+                if (x.Address.Count > 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }*/
+    }
+}
---
+++ LinqAndLambdaSolution/LinqUI/SampleData.cs
@@ -0,0 +1,42 @@
+﻿using LinqUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUI
+{
+    public static class SampleData
+    {
+        public static List<ContactModel> GetContactData()
+        {
+            List<ContactModel> output = new List<ContactModel>
+            {
+                new ContactModel{Id = 1, FirstName = "Paul", LastName = "Revill", Address = new List<int>{1, 2, 3,}},
+                new ContactModel{Id = 2, FirstName = "Coral", LastName = "Murphy", Address = new List<int>{1,}},
+                new ContactModel{Id = 3, FirstName = "Joe", LastName = "Burns", Address = new List<int>{2,}},
+                new ContactModel{Id = 4, FirstName = "Michael", LastName = "Dean", Address = new List<int>{3,}},
+                new ContactModel{Id = 5, FirstName = "Lloyd", LastName = "Chadwick", Address = new List<int>{2, 3,}}
+            };
+
+            return output;
+        }
+
+        public static List<AddressModel> GetAddressData()
+        {
+            List<AddressModel> output = new List<AddressModel>
+            {
+                new AddressModel{Id = 1, ContactId = 1, City = "Stockport", State = "GM" },
+                new AddressModel{Id = 2, ContactId = 1, City = "Leeds", State = "YS" },
+                new AddressModel{Id = 3, ContactId = 2, City = "Wolves", State = "BH" },
+                new AddressModel{Id = 4, ContactId = 5, City = "Stockport", State = "GM" },
+                new AddressModel{Id = 5, ContactId = 5, City = "Leeds", State = "YS" },
+                new AddressModel{Id = 6, ContactId = 4, City = "Leeds", State = "YS" },
+                new AddressModel{Id = 7, ContactId = 3, City = "Wolves", State = "BH" }
+            };
+
+            return output;
+        }
+    }
+}
