0b05a27f4f0525ebdb5d3936e5eeea9d80580fc7
PaulRevillIgnite <paul.revill@ignite.systems>
2025-10-15 15:12:21 +0100

    HotelApp - Get avaliable rooms

---
+++ HotelApp/DataAccess/Data/SqlData.cs
@@ -0,0 +1,28 @@
+﻿using HotelAppLibary.Databases;
+using HotelAppLibary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppLibary.Data
+{
+    public class SqlData
+    {
+        private readonly ISqlDataAccess db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            this.db = db;
+        }
+        public List<RoomTypeModel> GetAvaliableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            return db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvaliableTypes",
+                                                new { startDate, endDate },
+                                                connectionStringName,
+                                                true);
+        }
+    }
+}
